fe082f317b96c9f5cf952e35c1d4dfb673354071
aadhya-arora <aadhya270805@gmail.com>
2025-06-14 02:29:38 +0530

    integrated api

--- AI-Tool/src/App.jsx
+++ AI-Tool/src/App.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import "./App.css";
-
+import { URL } from "./constants";
 function App() {
+  const [question, setQuestion] = useState("");
+  const [result, setResult] = useState("");
+  const payload = {
+    contents: [
+      {
+        parts: [
+          {
+            text: question,
+          },
+        ],
+      },
+    ],
+  };
+  const askQuestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    response = await response.json();
+    //console.log(response.candidates[0].content.parts[0]);
+    setResult(response.candidates[0].content.parts[0].text);
+  };
   return (
     <div className="page">
-      <div className="nav"></div>
+      <div className="nav">
+        <div className="history">
+          <p>Search History</p>
+        </div>
+      </div>
       <div className="main">
-        <div className="container"></div>
+        <div className="container">{result}</div>
         <div className="question">
-          <input type="text" placeholder="Ask me anything" className="typing" />
-          <button className="ask">Ask</button>
+          <input
+            type="text"
+            value={question}
+            onChange={(event) => setQuestion(event.target.value)}
+            placeholder="Ask me anything"
+            className="typing"
+          />
+          <button onClick={askQuestion} className="ask">
+            Ask
+          </button>
         </div>
       </div>
     </div>
---
+++ AI-Tool/src/constants.js
@@ -0,0 +1,2 @@
+export const URL =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDg0Hxl-o2-xWSF97SNBPr8dH8gD66OFvA";
