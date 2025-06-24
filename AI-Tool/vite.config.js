632d35eb0f7cb6c89b8b4e76e799fdb4766c734b
aadhya-arora <aadhya270805@gmail.com>
2025-06-24 14:03:16 +0530

    deploying

--- AI-Tool/vite.config.js
+++ AI-Tool/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../dist", // build folder outside src
+    outDir: "dist", // build folder outside src
     emptyOutDir: true,
   },
   resolve: {
