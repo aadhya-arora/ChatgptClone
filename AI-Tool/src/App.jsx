a63b2e141ec15b39dc14b9af1a698ac0a786e7d8
aadhya-arora <aadhya270805@gmail.com>
2025-06-24 16:49:21 +0530

    ..

--- AI-Tool/src/App.jsx
+++ AI-Tool/src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Front from "./components/Front";
 import AuthForm from "./components/AuthForm";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route path="/front" element={<Front />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
